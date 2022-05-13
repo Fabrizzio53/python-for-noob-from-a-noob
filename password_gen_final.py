371aed75bc87e829cb57c57a0c89d63706fa5481
Fabrizzio53 <88493503+Fabrizzio53@users.noreply.github.com>
2022-05-12 21:55:48 -0300

    Update password_gen_final.py

--- password_gen_final.py
+++ password_gen_final.py
@@ -10,34 +10,24 @@ special = list("!@#$%^&*()")
 def generate_random_password():
 
     finalnumber = abs(int(input("Type the max number of your password: ")))
-    while finalnumber > 71 or finalnumber == 0:
-        print("Password must be less that 71 characters and more than 0")
-        finalnumber = abs(int(input("Type the max number of your password: ")))
 
     asciinumber = abs(int(input("type the number of letters: ")))
 
-    while asciinumber > 52 or asciinumber == 0:
-        print("The maximum number allowed is 52, please type something less than 52 and more than 0")
-        asciinumber = abs(int(input("type the number of letters: ")))
-
     digitsnumber = abs(int(input("Type the number of digits: ")))
 
-    while digitsnumber > 9:
-        print("The maximum number allowed is 9, please type somethin less than 9")
-        digitsnumber = abs(int(input("Type the number of digits: ")))
-
     specialnumber = abs(int(input("Type the number of special characters you want: ")))
-
-    while specialnumber > 10:
-        print("The maximum number allowed is 10, please type somethin less than 10")
-        specialnumber = abs(int(input("Type the number of special characters you want: ")))
 
     password = []
 
     while finalnumber < asciinumber + digitsnumber + specialnumber:
         print("The max number is less than the lenght of ascii, digits and special, type again")
-        print("The number of digits: ", asciinumber + digitsnumber + specialnumber)
-        finalnumber = int(input("Type the max number of your password: "))
+        finalnumber = abs(int(input("Type the max number of your password: ")))
+
+        asciinumber = abs(int(input("type the number of letters: ")))
+
+        digitsnumber = abs(int(input("Type the number of digits: ")))
+
+        specialnumber = abs(int(input("Type the number of special characters you want: ")))
     
     random.shuffle(ascii)
     random.shuffle(digits)
